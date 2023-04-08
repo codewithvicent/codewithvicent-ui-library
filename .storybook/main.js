10a9681a4b9934d18dd89f6611ceae9cc8ebd9b6
Abeinevincent <abeinevincent@gmail.com>
2023-04-08 08:15:32 +0300

    fix: fixed storybook update to version 7 semantics"

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,19 @@
 module.exports = {
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@chakra-ui/storybook-addon', "@storybook/addon-mdx-gfm"],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
-    check: true, // type-check stories during Storybook build
+    check: true // type-check stories during Storybook build
+  },
+
+  features: {
+    emotionAlias: false
+  },
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
   }
 };
